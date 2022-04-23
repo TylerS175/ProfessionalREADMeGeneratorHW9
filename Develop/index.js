7dc90c94d5da7f13ced15b90a61505799f7008ea
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-04-23 19:15:40 -0400

    Questionarie beta

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,23 @@
 // Creating mult variables 
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 const fs = require('fs');
+
+//Creating survey of questions 
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter your title!');
+                return false;
+            }
+        }
+    },
+    
+]
 

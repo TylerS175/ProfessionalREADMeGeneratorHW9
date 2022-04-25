697f384f766f157b365df92ad20730d3d6a37262
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-04-24 21:09:21 -0400

    Series of Questions

--- Develop/index.js
+++ Develop/index.js
@@ -33,9 +33,123 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'What',
+        name: 'email',
         message: 'What is your email address? (Required) ',
-        validate: github
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'what',
+        message: 'What is your porject and what problem problem will it solve? (Required)',
+        validate: whatInput => {
+            if (whatInput) {
+                return true;
+            } else {
+                console.log('Please enter what your project is!');
+                return false;
+            }
+        }
+    },
+    {   
+        type: 'input',
+        name: 'why',
+        message: 'Why did you create this project? (Required)',
+        validate: whyInput => {
+            if (whyInput) {
+                return true;
+            } else {
+                console.log('Please enter why you created this project!');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'how',
+        message: 'How will someone use this? (Required)',
+        validate: howInput => {
+            if (howInput) {
+                return true;
+            } else {
+                console.log('Please enter what your project is!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide step-by-step installation instructions for your project. (Required)',
+        validate: installInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter your use instructions!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions and examples for use (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log ('Please enter your use instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license will you use for your project?',
+        choices: ['agpl', 'apache', 'mit', 'no license']
+    },
+    {
+        type: 'confirm',
+        name: 'confirmcontributers',
+        message: 'Would you like to allow other developers to contribute?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Would you like to allow other developers to contribute?',
+        when: ({ confirmContributers}) => {
+            if (confirmContributers) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: contributerInput => {
+            if (contributerInput) {
+                return true;
+            } else {
+                console.log('Please enter contributer guidelines!');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide instruction on how to test the app. (Required)',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please enter your use test instructions!');
+                return false; 
+            }
+        }
     }
-]
+];
 
